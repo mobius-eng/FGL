29d2faea3b2f2d8fac1e2119e26d503db2006a3f
mobius-eng <mobius-eng@users.noreply.github.com>
2025-07-10 14:52:41 +0200

    update bitsting test app

--- BitStringTest/BitStringDemo.f90
+++ BitStringTest/BitStringDemo.f90
@@ -1,22 +1,37 @@
-program bitstring_demo
+PROGRAM bitstring_demo
     
-  use bitstring_m
+  USE bitstring_m
   
-  implicit none
+  IMPLICIT NONE
   
-  type(bitstring_t) :: bs, bs2
-  logical, dimension(:), allocatable :: larray
+  TYPE(bitstring_t) :: bs, bs2
+  LOGICAL, ALLOCATABLE :: larray(:)
+  CHARACTER(64), PARAMETER :: fmt_markers =   &
+    '(T4,A1,T8,A1,T12,A2,T17,A2,T22,A2,T27,A2)'
   
-  bs = make_zero_bitstring(35)
-  write (*,*) 'Zero: ', bs
-  call bs%set(1, 1)
-  call bs%set(4, 1)
-  call bs%set(34, 1)
-  write (*,*) '1 in positions 1, 4 & 34: ', bs
+  WRITE (*, '(T20,A)') 'FGL: BitString testing'
+  WRITE (*, '(A)') REPEAT('=', 72)
+  WRITE (*, '(T4,A,/)') 'BitString: efficient implementation of 0-1 arrays'
+  
+  WRITE (*, '(T2,A)') 'Creating zero bit-string of length 25'
+  bs = make_zero_bitstring(25)
+  WRITE (*, fmt=fmt_markers) '1', '5', '10', '15', '20', '25'
+  WRITE (*, fmt=fmt_markers) '|', '|', ' |', ' |', ' |', ' |'
+  WRITE (*,'(T4,DT)') bs
+  WRITE(*, '(T2,A)') 'Setting bits at positions 1, 4, 24'
+  CALL bs%set(1, 1)
+  CALL bs%set(4, 1)
+  CALL bs%set(24, 1)
+  WRITE (*, fmt=fmt_markers) '1', '5', '10', '15', '20', '25'
+  WRITE (*, fmt=fmt_markers) '|', '|', ' |', ' |', ' |', ' |'
+  WRITE (*,'(T4,DT)') bs
+  WRITE (*,'(T4,25L1)') bs%convert_to_logical_array()
   larray = bs%convert_to_logical_array()
-  write (*,*) bs%convert_to_logical_array()
-  larray(5) = .true.
+  larray(10) = .TRUE.
+  WRITE (*, '(T2,A)') 'Convert LOGICAL LEN=16 with set bits: 1, 4, 10'
   bs2 = make_bitstring_from_logical_array(larray(1:16))
-  write (*,'(dt)') bs2
+  WRITE (*, fmt=fmt_markers) '1', '5', '10', '15', '20', '25'
+  WRITE (*, fmt=fmt_markers) '|', '|', ' |', ' |', ' |', ' |'
+  WRITE (*,'(T4,DT)') bs2
     
-end program
+END PROGRAM
